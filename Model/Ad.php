1b2b44f0769e17ff647edc5ad1dc37515b3c382a
krazydanny <danielspadafora@gmail.com>
2017-02-23 15:54:54 -0300

    redis test

--- Model/Ad.php
+++ Model/Ad.php
@@ -60,7 +60,7 @@
 			//test
 			$this->_geolocation->detect( $ip );
 			$this->_deviceDetection->detect( $userAgent );			
-			$this->_registry->adCode = $demand['ad_code'];
+			$this->_registry->adCode = 'algo';
 			$this->_registry->status = 200;
 			return true;
 			//-------------------------------------
