203521875086fea25e31c514093b69084a431fa2
krazydanny <danielspadafora@gmail.com>
2017-02-23 18:24:46 -0300

    redis test

--- Model/Ad.php
+++ Model/Ad.php
@@ -38,7 +38,7 @@
 			//-------------------------------------
 			// ADD TEST DATA
 			//-------------------------------------			
-			/*
+
 			$this->_deviceDetection->detect( $userAgent );
 
 			$this->_geolocation->detect( $ip );
@@ -57,7 +57,7 @@
 				'carrier'		  => $this->_geolocation->getMobileCarrier(),
 				'os'			  => $this->_deviceDetection->getOs()
 			)));
-			*/
+
 
 			//-------------------------------------
 			// MATCH SUPPLY (placement_id)
