e3ab75a58be2c3083b9ee290c8061432e18551d7
Hipercubus <subespacios@gmail.com>
2017-04-27 21:33:51 -0300

    set iframe

--- View/ad.php
+++ View/ad.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<body>
+<body style="margin:0;padding:0;">
 <a href=" <?php echo $registry->landingUrl; ?> "><img src="<?php echo $registry->creativeUrl; ?>"/></a>
 <?php echo $registry->adCode; ?>
 </body>
