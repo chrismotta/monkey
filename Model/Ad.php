185371d540a06ebecd801a67bf62348880ae5a4c
krazydanny <danielspadafora@gmail.com>
2017-05-02 17:42:50 -0300

    fix

--- Model/Ad.php
+++ Model/Ad.php
@@ -89,8 +89,8 @@
 			}
 			else
 			{
-				echo '<!-- ip: '.$ip.' -->';
-				echo '<!-- user agent: '.$userAgent.' -->';
+				//echo '<!-- ip: '.$ip.' -->';
+				//echo '<!-- user agent: '.$userAgent.' -->';
 				$sessionHash = \md5( 
 					\date( 'Y-m-d', $timestamp ) .
 					$placement['cluster_id'] .
@@ -109,10 +109,10 @@
 			$clusterImpCount = $this->_cache->getMapField( 'clusterlog:'.$sessionHash, 'imps' );
 			$logWasTargetted = $this->_cache->getMapField( 'clusterlog:'.$sessionHash, 'targetted' );
 
-			echo '<!-- placement status: '.$placement['status'].' -->';
-			echo '<!-- placement imps: '.$placement['imps'].' -->';
-			echo '<!-- cluster imps: '.$clusterImpCount.' -->';
-			echo '<!-- process tracking: -->';
+			//echo '<!-- placement status: '.$placement['status'].' -->';
+			//echo '<!-- placement imps: '.$placement['imps'].' -->';
+			//echo '<!-- cluster imps: '.$clusterImpCount.' -->';
+			//echo '<!-- process tracking: -->';
 			if (
 				$placement['status'] == 'health_check' 
 				|| $placement['status'] == 'testing' 
@@ -125,12 +125,12 @@
 				// if cluster log already exists increment, otherwise create new
 				if ( $clusterImpCount )
 				{
-					echo '<!-- no retargeting => increment log -->';
+					//echo '<!-- no retargeting => increment log -->';
 					$this->_incrementClusterLog( $sessionHash, $placement, $clusterImpCount, $timestamp );
 				}
 				else
 				{
-					echo '<!-- no retargeting => new log -->';
+					//echo '<!-- no retargeting => new log -->';
 					$device = $this->_getDeviceData( $userAgent );
 					$this->_geolocation->detect( $ip );
 
@@ -155,11 +155,11 @@
 
 				$this->_geolocation->detect( $ip );
 
-				echo '<!-- cs -->';
+				//echo '<!-- cs -->';
 
 				if ( $this->_matchClusterTargeting( $cluster, $device ) )
 				{
-					echo '<!-- => matched cluster cs -->';
+					//echo '<!-- => matched cluster cs -->';
 					$detectionSuccess = $this->_fraudDetection->analize([
 						'request_type'	=> 'display',
 						'ip_address'	=> $ip,
@@ -170,7 +170,7 @@
 					// if fraud detection passes, log and do retargeting
 					if ( $detectionSuccess && $this->_fraudDetection->getRiskLevel() < Config\Ad::FRAUD_RISK_LVL )
 					{
-						echo '<!-- => passed fraud detection -->';
+						//echo '<!-- => passed fraud detection -->';
 						$this->_newClusterLog ( $sessionHash, $timestamp, $ip, $placement, $device, $placement_id, true );
 
 						$campaigns = $this->_cache->getSet( 'clusterlist:'.$placement['cluster_id'] );
@@ -184,7 +184,7 @@
 							$this->_newCampaignLog( $clickId, $sessionHash, $campaignId, $timestamp );	
 						}
 
-						echo '<!-- <br>click IDs:'.json_encode($clickIDs) . ' -->';
+						////echo '<!-- <br>click IDs:'.json_encode($clickIDs) . ' -->';
 
 						// run campaign selection with retargeting
 						$this->_campaignSelection->run( $clickIDs );
@@ -215,7 +215,7 @@
 
 			// pass sid for testing
 			//$this->_registry->sid = $sessionHash;
-			echo '<!-- session_hash: '.$sessionHash.' -->';
+			//echo '<!-- session_hash: '.$sessionHash.' -->';
 			// Tell controller process completed successfully
 			$this->_registry->status = 200;
 			return true;
@@ -331,7 +331,7 @@
 			if ( !$data )
 			{
 				$this->_deviceDetection->detect( $ua );
-				echo '<!-- using device detector: yes -->';
+				//echo '<!-- using device detector: yes -->';
 				$data = array(
 					'os' 			  => $this->_deviceDetection->getOs(),
 					'os_version'	  => $this->_deviceDetection->getOsVersion(), 
