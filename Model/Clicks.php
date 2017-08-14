f99a2eca6068b3c4b0bd40b3c957e72463938f3a
krazydanny <danielspadafora@gmail.com>
2017-08-14 12:53:41 -0300

    click fix

--- Model/Clicks.php
+++ Model/Clicks.php
@@ -31,12 +31,13 @@
 
 			if ( $campaignLog )
 			{
+
 				$this->_cache->setMapField( 'campaignlog:'. $click_id, 'click_time', $this->_registry->httpRequest->getTimestamp() );
 
 				$this->_cache->addToSortedSet( 'clickids', $this->_registry->httpRequest->getTimestamp(), $click_id );
 
 				$this->_cache->useDatabase( 0 );
-				$callbackURL = $this->_cache->getMapField( 'campaign:'.$campaignLog['campaign_id'], 'callback' ) . '&click_id='.$click_id;
+				$callbackURL = $this->_cache->getMapField( 'campaign:'.$campaignLog['campaign_id'], 'callback' ) . '&aff_sub='.$click_id;
 
 				//-------------------------------------
 				// NOTIFY AFFILIATE  
@@ -44,11 +45,9 @@
 	        	$httpClient 	   = new Framework\TCP\HTTP\Client\cURL();
 				$httpClientRequest = new Framework\TCP\HTTP\Client\Request();
 
-				$httpClientRequest->setURL( $url );
+				$httpClientRequest->setURL( $callbackURL );
 
-				$r = $httpClient->send( $httpClientRequest );
-
-				var_export($r);
+				$httpClient->send( $httpClientRequest );
 			}
 
 
