5523d785930f85813dd8a130a6c20ed774db5e21
Hipercubus <subespacios@gmail.com>
2017-09-11 10:53:14 -0300

    target blank

--- View/html.php
+++ View/html.php
@@ -2,7 +2,7 @@
 <html>
 <head></head>
 <body style="margin:0;padding:0">
-<a href="<?php echo $registry->landingUrl; ?>" target="_top"><img src="<?php echo $registry->creativeUrl; ?>"/></a>
+<a href="<?php echo $registry->landingUrl; ?>" target="_blank"><img src="<?php echo $registry->creativeUrl; ?>"/></a>
 <?php echo $registry->adCode; ?>
 </body>
 </html>
