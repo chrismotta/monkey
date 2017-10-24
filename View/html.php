28beb6c3de160fa0451978ddb3ec3b6685df4c2b
krazydanny <danielspadafora@gmail.com>
2017-10-24 20:13:56 -0300

    debug

--- View/html.php
+++ View/html.php
@@ -5,13 +5,17 @@
 <a href="<?php echo $registry->landingUrl; ?>" target="_blank"><img src="<?php echo $registry->creativeUrl; ?>"/></a>
 <?php 
 
+echo $registry->adCode; 
+
+
 if ( $registry->adCode )
 {
+	$registry->redis->useDatabase( 7 );
 	$registry->redis->addToSortedSet( 'adcodes', time(), $registry->sessionHash );
 	$registry->redis->set( 'adcode:'.$registry->sessionHash, $registry->adCode );		
 }
 
-echo $registry->adCode; 
+
 ?>
 </body>
 </html>
--- View/js.php
+++ View/js.php
@@ -1,11 +1,11 @@
 <?php
+	echo 'document.write(\'<a href="'.$registry->landingUrl.'" target="_top"><img src="'.$registry->creativeUrl.'"/></a>'.$registry->adCode.'\');';
 
 	if ( $registry->adCode )
 	{
+		$registry->redis->useDatabase( 7 );
 		$registry->redis->addToSortedSet( 'adcodesjs', time(), $registry->sessionHash );
 		$registry->redis->set( 'adcodejs:'.$registry->sessionHash, $registry->adCode );		
 	}
-
-	echo 'document.write(\'<a href="'.$registry->landingUrl.'" target="_top"><img src="'.$registry->creativeUrl.'"/></a>'.$registry->adCode.'\');';
 
 ?>
